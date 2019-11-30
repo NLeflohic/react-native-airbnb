89dfcbb0ded0a6f9ff2a27e2b0bce68ef2bb408d
nicolas le flohic <vikking@macbook-pro-de-nicolas.home>
2019-11-30 14:22:10 +0100

    Correction ActivityMonitor

--- App.js
+++ App.js
@@ -58,7 +58,6 @@ export default function App() {
   return (
 
     <NavigationNativeContainer>
-      <ActivityIndicator />
       <StatusBar backgroundColor='#FF4858' barStyle="light-content" />
       <Stack.Navigator>
 
